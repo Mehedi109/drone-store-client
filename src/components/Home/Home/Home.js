9a862e86fbeb5819ce729c2b5e37e30b6b0ee241
Mehedi Alam <mehedialam109@gmail.com>
2022-01-09 02:33:32 +0600

    contact added

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import Footer from '../../Shared/Footer/Footer';
-import Header from '../../Shared/Header/Header';
-import About from '../About/About';
-import Banner from '../Banner/Banner';
-import Products from '../Products/Products';
-import Reviews from '../Reviews/Reviews';
+import React from "react";
+import Footer from "../../Shared/Footer/Footer";
+import Header from "../../Shared/Header/Header";
+import About from "../About/About";
+import Banner from "../Banner/Banner";
+import Contact from "../Contact/Contact";
+import Products from "../Products/Products";
+import Reviews from "../Reviews/Reviews";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Products></Products>
       <About></About>
       <Reviews></Reviews>
+      <Contact></Contact>
       <Footer></Footer>
     </div>
   );
